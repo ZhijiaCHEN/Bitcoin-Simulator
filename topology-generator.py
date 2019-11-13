7bda1c4fc577c6cfa9ed669df7f61d396241c86c
Zhijia Chen <zhijia.chen@temple.edu>
2019-11-12 19:41:54 -0500

    .

--- topology-generator.py
+++ topology-generator.py
@@ -10,6 +10,8 @@ parser.add_argument('-p', default=0.5, help='the probability for each pair of no
 parser.add_argument('-o', default='topo.json', help='out put file')
 regions = [0, 1, 2, 3, 4, 5]
 args = parser.parse_args()
+args.n = int(args.n)
+args.p = float(args.p)
 topo = {"nodes":[]}
 for i in range(args.n):
     node = {"id": i, "region": random.sample(regions, 1)[0], "isMiner": True, "hashRate": random.random(), "neighbor":[]}
